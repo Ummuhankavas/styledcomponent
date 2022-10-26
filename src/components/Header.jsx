122f1fc799fce985bda543e2cc1caf98c9897ed0
Ummuhankavas <ummuhankavas@gmail.com>
2022-10-26 09:57:49 +0200

    btn styled added

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Button from "./styles/Button.styled";
+
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <Button color='#A62440'>Aply Courses</Button>
+            <Button bg='#A62440'>Talk to Adviser</Button>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
